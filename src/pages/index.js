4c7921e21485a73292918044fdf474af694f4334
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2020-12-14 00:36:09 +0900

    맵 html

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ import withSplitting from 'withSplitting';
 export const Main = withSplitting(() => import('./Main'));
 export const Detail = withSplitting(() => import('./Detail'));
 export const Item = withSplitting(() => import('./Item'));
+export const Map = withSplitting(() => import('./Map'));
