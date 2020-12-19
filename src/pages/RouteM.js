453890f3f54185e9d3e477f0709d91866dcfd737
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2020-12-19 22:33:21 +0900

    루트제작 추가 디자인

--- src/pages/RouteM.js
+++ src/pages/RouteM.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { injectIntl  } from 'react-intl';
 import ScriptTag from 'react-script-tag';
 import { Header, SubBanner, AdS, Footer } from 'components/banner'
-import map from 'img/map.png';
+import map from 'img/map2.png';
 
 class Map extends Component {
     constructor(props) {
@@ -328,7 +328,59 @@ class Map extends Component {
                             <span className="Route_R_stat_title">능력치</span>
                         </div>
                         <div className="Route_R_Map">
-                            <img className="Route_R_Mapimg" src={map} /> 
+                            <div className="Route_R_Mapimg_box">
+                                <img className="Route_R_Mapimg" src={map} /> 
+                                <img className="Route_R_Mapimg_archery actived" src='img/map/양궁장.png' /> 
+                                <img className="Route_R_Mapimg_alley" src='img/map/골목길.png' /> 
+                                <img className="Route_R_Mapimg_school" src='img/map/학교.png' /> 
+                                <img className="Route_R_Mapimg_hotel" src='img/map/호텔.png' /> 
+                                <img className="Route_R_Mapimg_beach" src='img/map/모래사장.png' /> 
+                                <img className="Route_R_Mapimg_forest" src='img/map/숲.png' /> 
+                                <img className="Route_R_Mapimg_uptown" src='img/map/고급주택가.png' /> 
+                                <img className="Route_R_Mapimg_pond" src='img/map/연못.png' /> 
+                                <img className="Route_R_Mapimg_temple" src='img/map/절.png' /> 
+                                <img className="Route_R_Mapimg_hospital" src='img/map/병원.png' /> 
+                                <img className="Route_R_Mapimg_chapel" src='img/map/성당.png' /> 
+                                <img className="Route_R_Mapimg_factory" src='img/map/공장.png' /> 
+                                <img className="Route_R_Mapimg_dock" src='img/map/항구.png' /> 
+                                <img className="Route_R_Mapimg_cemetery" src='img/map/묘지.png' /> 
+                                <img className="Route_R_Mapimg_avenue" src='img/map/번화가.png' /> 
+                                <div className="Route_R_Mapspan_box">
+                                    <span className="Route_R_Mapspan_archery actived"> {intl.formatMessage({id: 'Archery'})} </span>
+                                    <span className="Route_R_Mapspan_alley"> {intl.formatMessage({id: 'Alley'})} </span>
+                                    <span className="Route_R_Mapspan_school"> {intl.formatMessage({id: 'School'})} </span>
+                                    <span className="Route_R_Mapspan_hotel"> {intl.formatMessage({id: 'Hotel'})} </span>
+                                    <span className="Route_R_Mapspan_beach"> {intl.formatMessage({id: 'Beach'})} </span>
+                                    <span className="Route_R_Mapspan_forest"> {intl.formatMessage({id: 'Forest'})} </span>
+                                    <span className="Route_R_Mapspan_uptown"> {intl.formatMessage({id: 'Uptown'})} </span>
+                                    <span className="Route_R_Mapspan_pond"> {intl.formatMessage({id: 'Pond'})} </span>
+                                    <span className="Route_R_Mapspan_temple"> {intl.formatMessage({id: 'Temple'})} </span>
+                                    <span className="Route_R_Mapspan_hospital"> {intl.formatMessage({id: 'Hospital'})} </span>
+                                    <span className="Route_R_Mapspan_chapel"> {intl.formatMessage({id: 'Chapel'})} </span>
+                                    <span className="Route_R_Mapspan_factory"> {intl.formatMessage({id: 'Factory'})} </span>
+                                    <span className="Route_R_Mapspan_dock"> {intl.formatMessage({id: 'Dock'})} </span>
+                                    <span className="Route_R_Mapspan_cemetery"> {intl.formatMessage({id: 'Cemetery'})} </span>
+                                    <span className="Route_R_Mapspan_avenue"> {intl.formatMessage({id: 'Avenue'})} </span>
+                                    <span className="Route_R_Mapspan_research"> {intl.formatMessage({id: 'Research'})} </span>
+                                    
+                                    <span className="Route_R_Mapspan_archery1 actived"> {intl.formatMessage({id: '1'})} </span>
+                                    <span className="Route_R_Mapspan_alley1"> {intl.formatMessage({id: '2'})} </span>
+                                    <span className="Route_R_Mapspan_school1"> {intl.formatMessage({id: '3'})} </span>
+                                    <span className="Route_R_Mapspan_hotel1"> {intl.formatMessage({id: '4'})} </span>
+                                    <span className="Route_R_Mapspan_beach1"> {intl.formatMessage({id: '5'})} </span>
+                                    <span className="Route_R_Mapspan_forest1"> {intl.formatMessage({id: '6'})} </span>
+                                    <span className="Route_R_Mapspan_uptown1"> {intl.formatMessage({id: '7'})} </span>
+                                    <span className="Route_R_Mapspan_pond1"> {intl.formatMessage({id: '1'})} </span>
+                                    <span className="Route_R_Mapspan_temple1"> {intl.formatMessage({id: '2'})} </span>
+                                    <span className="Route_R_Mapspan_hospital1"> {intl.formatMessage({id: '3'})} </span>
+                                    <span className="Route_R_Mapspan_chapel1"> {intl.formatMessage({id: '4'})} </span>
+                                    <span className="Route_R_Mapspan_factory1"> {intl.formatMessage({id: '5'})} </span>
+                                    <span className="Route_R_Mapspan_dock1"> {intl.formatMessage({id: '6'})} </span>
+                                    <span className="Route_R_Mapspan_cemetery1"> {intl.formatMessage({id: '7'})} </span>
+                                    <span className="Route_R_Mapspan_avenue1"> {intl.formatMessage({id: '1'})} </span>
+                                    <span className="Route_R_Mapspan_research1"> {intl.formatMessage({id: '2'})} </span>
+                                </div>
+                            </div>
                             <div className="Route_R_Mapitem_allbox">
                                 <span className="Route_R_Mapitem_title">드랍 아이템</span>
                                 <div className="Route_R_Mapitem_box">
