e6bee3ab2b6221412bb90c290e0c1d40af163169
동원 김 <dongwon412@mirageworks.co.kr>
2020-12-30 20:22:48 +0900

    크로스 도메인

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const port = 3001;
 const connect = require('./schemas');
 const rank = require('./routes/Rank');
@@ -10,6 +11,7 @@ const app = express();
 
 connect();
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use('/api/rank', rank);
 app.use('/api/user', users);
