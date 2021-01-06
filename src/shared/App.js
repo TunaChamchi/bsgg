9a9946f83a960eacf4fe508397cd17a56a57c78b
동원 김 <dongwon412@mirageworks.co.kr>
2021-01-06 15:32:41 +0900

    오류 수정

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { Main, Detail, Map, RouteM, NewMain ,Match, Character, Rank, Rank_Character, Error404 } from 'pages';
 
 class App extends Component {
@@ -21,7 +21,6 @@ class App extends Component {
                     <Route path="/RankCharacter" component={Rank_Character}/>
 
                     <Route path="/404" component={Error404}/>
-                    <Redirect to="/404" />
                 </Switch>
             </div>
         );
