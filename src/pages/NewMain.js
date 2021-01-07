99bd1ffa8adae754e9649c6cdee2565eaa1e652a
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2021-01-07 23:29:17 +0900

    광고 오류 수정

--- src/pages/NewMain.js
+++ src/pages/NewMain.js
@@ -165,7 +165,7 @@ class Main extends Component {
                         className='mainpage_ad'
                         client='ca-pub-7215780243476450'
                         slot='1329310229'
-                        style={{ display: 'block', width:970, height:250 }}
+                        style={{ display: 'inline-block', width:970, height:250 }}
                         responsive='true'
                         />
                 </div>
