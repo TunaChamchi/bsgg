090e48a923b926805260de8db688de00df747366
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2021-01-16 21:13:43 +0900

    통계 디자인 작업

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export const Match = withSplitting(() => import('./match'));
 export const Character = withSplitting(() => import('./character'));
 export const Rank = withSplitting(() => import('./rank'));
 export const Rank_Character = withSplitting(() => import('./rank_Character'));
+export const Stat = withSplitting(() => import('./Stat'));
 export const Talk = withSplitting(() => import('./Talk'));
 export const Talk2 = withSplitting(() => import('./Talk2'));
 export const Talk3 = withSplitting(() => import('./Talk3'));
