591945026a8a6963aefcd7772164d68d6b0cb450
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2021-01-17 17:54:39 +0900

    css 수정 , 통계 페이지 작업

--- src/pages/Stat.js
+++ src/pages/Stat.js
@@ -27,6 +27,11 @@ class Map extends Component {
                 <SubBanner actived={'Route'} />
                     <div className="stat">
                         <div className="stat_span">Statistics</div>
+                        <div className="stat_span_tabs">
+                            <div className="stat_span_tab actived">솔로</div>
+                            <div className="stat_span_tab">듀오</div>
+                            <div className="stat_span_tab">스쿼드</div>
+                        </div>
                         <div className="stat_left">
                             <div className="stat_left_bar">
                                 <div className="stat_left_bar_span">분포도 그래프</div>
@@ -84,6 +89,86 @@ class Map extends Component {
                                         <div className="stat_left_bar_graph_span2">다이아몬드</div>
                                         <div className="stat_left_bar_graph_span2">데미갓</div>
                                         <div className="stat_left_bar_graph_span2">이터니티</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="stat_left_server">
+                                <div className="stat_left_server_span">서버 분포도</div>
+                                <div className="stat_left_server_tabs">
+                                    <div className="stat_left_server_tab actived">한국</div>
+                                    <div className="stat_left_server_tab">홍콩</div>
+                                    <div className="stat_left_server_tab">북미</div>
+                                    <div className="stat_left_server_tab">유럽</div>
+                                </div>
+                                <div className="stat_left_server_circle_box">
+                                    <div className="stat_left_server_circle"></div>
+                                    <div className="stat_left_server_circle_spans">
+                                        <div className="stat_left_server_circle_span_kr">한국 50.2%</div>
+                                        <div className="stat_left_server_circle_span_hk">홍콩 50.2%</div>
+                                        <div className="stat_left_server_circle_span_us">북미 50.2%</div>
+                                        <div className="stat_left_server_circle_span_eu">유럽 50.2%</div>
+                                        <div className="stat_left_server_circle_span_kr">한국 50.2%</div>
+                                        <div className="stat_left_server_circle_span_hk">홍콩 50.2%</div>
+                                        <div className="stat_left_server_circle_span_us">북미 50.2%</div>
+                                        <div className="stat_left_server_circle_span_eu">유럽 50.2%</div>
+                                    </div>
+                                </div>
+                                <div className="stat_left_server_graphs_box">    
+                                    <div className="stat_left_server_graphs">
+                                        <div className="stat_left_bar_server_iron"></div>
+                                        <div className="stat_left_bar_server_iron"></div>
+                                        <div className="stat_left_bar_server_iron"></div>
+                                        <div className="stat_left_bar_server_iron"></div>
+                                        <div className="stat_left_bar_server_bronze"></div>
+                                        <div className="stat_left_bar_server_bronze"></div>
+                                        <div className="stat_left_bar_server_bronze"></div>
+                                        <div className="stat_left_bar_server_bronze"></div>
+                                        <div className="stat_left_bar_server_silver"></div>
+                                        <div className="stat_left_bar_server_silver"></div>
+                                        <div className="stat_left_bar_server_silver"></div>
+                                        <div className="stat_left_bar_server_silver"></div>
+                                        <div className="stat_left_bar_server_gold"></div>
+                                        <div className="stat_left_bar_server_gold"></div>
+                                        <div className="stat_left_bar_server_gold"></div>
+                                        <div className="stat_left_bar_server_gold"></div>
+                                        <div className="stat_left_bar_server_platinum"></div>
+                                        <div className="stat_left_bar_server_platinum"></div>
+                                        <div className="stat_left_bar_server_platinum"></div>
+                                        <div className="stat_left_bar_server_platinum"></div>
+                                        <div className="stat_left_bar_server_diamond"></div>
+                                        <div className="stat_left_bar_server_diamond"></div>
+                                        <div className="stat_left_bar_server_diamond"></div>
+                                        <div className="stat_left_bar_server_diamond"></div>
+                                        <div className="stat_left_bar_server_demigod"></div>
+                                        <div className="stat_left_bar_server_demigod"></div>
+                                        <div className="stat_left_bar_server_demigod"></div>
+                                        <div className="stat_left_bar_server_demigod"></div>
+                                        <div className="stat_left_bar_server_eternity"></div>
+                                        <div className="stat_left_bar_server_eternity"></div>
+                                        <div className="stat_left_bar_server_eternity"></div>
+                                        <div className="stat_left_bar_server_eternity"></div>
+                                    </div>
+                                    <div className="stat_left_bar_server_spans_all">
+                                        <div className="stat_left_bar_server_spans">
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                            <div className="stat_left_bar_server_span">4 3 2 1</div>
+                                        </div>
+                                        <div className="stat_left_bar_server_span2s">
+                                            <div className="stat_left_bar_server_span2">아이언</div>
+                                            <div className="stat_left_bar_server_span2">브론즈</div>
+                                            <div className="stat_left_bar_server_span2">실버</div>
+                                            <div className="stat_left_bar_server_span2">골드</div>
+                                            <div className="stat_left_bar_server_span2">플래티넘</div>
+                                            <div className="stat_left_bar_server_span2">다이아몬드</div>
+                                            <div className="stat_left_bar_server_span2">데미갓</div>
+                                            <div className="stat_left_bar_server_span2">이터니티</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -256,6 +341,7 @@ class Map extends Component {
                                     <div className="stat_right_span_tier2">25,555 (15.6%)</div>
                                     <div className="stat_right_span_tier3">251,555 (15.6%)</div>
                                 </div>
+                                <div className="stat_right_span_tier_bot"></div>    
                             </div>    
                         </div>
                     </div>
