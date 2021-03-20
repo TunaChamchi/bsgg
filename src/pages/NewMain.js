84856a2b8091fcce6fed1c5697cd80cbfab893c7
DESKTOP-TMNSMNQ\NZXT <donghyun9292>
2021-03-20 18:18:44 +0900

    신캐 공지 업뎃, 이미지 오류 수정

--- src/pages/NewMain.js
+++ src/pages/NewMain.js
@@ -150,7 +150,7 @@ class Main extends Component {
                         </div>
                     </div>
                 </div>
-                <Link to={"/Detail?character=24&bestWeapon=21"}>
+                <Link to={"/Detail?character=25&bestWeapon=11"}>
                     <div className="mainpage_notice">
                         <span className="mainpage_notice_span">{intl.formatMessage({id: '공지사항1'})}</span>
                     </div>
